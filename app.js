13b1fbcc39b7a3bba9bf2bf6a565019eb3276ae8
tylerwannamaker <37714246+tylerwannamaker@users.noreply.github.com>
2019-02-22 23:23:46 -0500

    Database init
    Added connection to database, config file for db user/pass/port, table creation.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const stockdatabase = require('./stock_modules/stockdb');
 
-var app = express();
 
+stockdatabase.connectdb();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
