72bc7ebcc90d1d8b59dca8086d51ec346539de98
RobertQuillian <47194488+RobertQuillian@users.noreply.github.com>
2019-03-03 18:00:51 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function HomeButton(){
+    window.open("http://localhost:3000/mainHomePage.html","_self");
+}
+function GoodStocksButton(){
+    window.open("http://localhost:3000/GoodStocks.html", "_self");
+}
+function BadStocksButton(){
+    window.open("http://localhost:3000/worstStock.html","_self");
+}
+function PopStocksButton(){
+    
+     //implement later
+}
+function BrowseIndButton(){
+     //implement later
+}
+function NewsButton(){
+    //implement later
+}
