4c339fde0792e6d5a40b4f99a1e4e7d2e39aab4d
tylerwannamaker <37714246+tylerwannamaker@users.noreply.github.com>
2019-03-03 20:20:23 -0500

    Front End + Stocks query fix

--- routes/stocks.js
+++ routes/stocks.js
@@ -5,16 +5,18 @@ const tools = require('../stock_modules/stocktools');
 router.get('/', function(req, res, next) {
     tools.showstocks(res);
 });
+router.get('/top', function(req, res, next) {
+    tools.showtop(res);
+});
+router.get('/bottom', function(req, res, next) {
+    tools.showbottom(res);
+});
+router.get('/:stockid', function(req, res) {
+    tools.showstock(req.params.stockid, res);
+});
 
 router.get('/:stockid/realtime', function(req, res) {
     tools.showraw(res, req.params.stockid);
 });
 
-router.get('/top', function(req, res, next) {
-    tools.showtop(res);
-});
-
-router.get('/bottom', function(req, res, next) {
-    tools.showbottom(res);
-});
 module.exports = router;
