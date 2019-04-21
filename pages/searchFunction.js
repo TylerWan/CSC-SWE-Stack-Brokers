07d166dbc6c22ee4e6e2b64b767caa8feb0b6f5f
RobertQuillian <47194488+RobertQuillian@users.noreply.github.com>
2019-04-21 19:29:12 -0400

    Add files via upload

--- pages/searchFunction.js
+++ pages/searchFunction.js
@@ -1,17 +1,18 @@
-function searchFunction(){
+function searchFunction() {
     var input = document.getElementById("searchBar").value;
     var n = input.length;
     var letters = /^[A-Za-z]+$/;
-    if(letters.test(input) == true){
-    if (n>5 | n==0) 
+    if(letters.test(input) == true)
+    {
+        if (n>5 | n==0) 
+        {
+        alert("Please Enter A Valid Ticker");
+        }
+    }
+    else
     {
        alert("Please Enter A Valid Ticker");
     }
-
-}
-   else{
-       alert("Please Enter A Valid Ticker");
-   }
  }
      var input = document.getElementById("searchBar");
      input.addEventListener("keyup", function(event){
